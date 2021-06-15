ad40989db4474084111d5fb7e966d4a513dcc3b8
Adithya <f20190181@goa.bits-pilani.ac.in>
2021-06-15 23:05:04 +0530

    Major changes

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -16,38 +16,6 @@ echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDKzn2gjOKERdwMYfxbc23KNy361TKxUbPANyX
 
 echo "Added SSH key"
 
-# changing the default password
-set password "password"
-
-sudo passwd root
-expect "New password:"
-send "$password"
-expect "Retype new password:"
-send "$password"
-
-echo "Changed password"
-
-# creating a temporary server
-cd ..
-mkdir temp-server
-cd temp-server
-
-# creating the node application
-npm init -y
-npm install express
-touch app.js
-
-# writing into the app.js file
-echo "const express = require('express');" >> app.js
-echo "const app = express();" >> app.js
-echo "app.get('/', function(req, res){ res.send('Server is running'); }); " >> app.js
-echo "app.listen(3000, function(){ console.log('Server is listening on port 3000'); });" >> app.js
-
-# starting up the server using pm2
-pm2 start app.js
-
-echo "Mock server started"
-
 # moving into the nginx directory
 cd /etc/nginx/sites-available
 rm default
@@ -68,25 +36,63 @@ sudo systemctl restart nginx
 echo "Nginx restarted"
 
 # creating the SSH key for GitHub
-set passphrase "github-vagrant"
-
 cd ~/.ssh
-ssh-keygen -t ed25519
-expect "Enter file in which to save the key (/root/.ssh/id_ed25519):"
-send "github"
-expect "Enter passphrase (empty for no passphrase):"
-send "$passphrase"
-expect "Enter same passphrase again:"
-send "$passphrase"
 
-mkdir ~/projectname
+echo "-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABlwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAYEAwb87s7UN9e7wy9vmYJ5ipxhGbuztwQ7l3LL+LE4QHvVHFYOSGexS
+zxpSbe4rI9W78gYIg0YZwyXESaAGaE2wo/tH5EIknC/qYLxOvty81LmtRnfcdn/4UzO4kI
+JqfFxEdmVY/B5JmOrSU0BcBut4gC1lw1pyEyT6VGvT8CBVlaPG4RWhvwepGKLSFWDr2jgk
+SVSywkvaGkt7ByOLpBusTHAsRQL4P2r6MuZ9GuR9QQUt5peciJpqozPicrhlXIEkckwK+m
+24Qvef9Fluqej8oXaGSnx99GUmhWFJPD+fDk1Nzw2WYC/qJV8NuFvCFqrQwyqd4i1fiIWR
+yN0/09DKT24cdO0QbZL3a2vFmHQ/ofNYi+2qOCptu7D9/SkYPZj1Rc5Vt2Vs5k8QtUEAI3
+qQTd8ZaJhtci6/tu7q0DhMI63KJlJ8l0so4oxll+5CNnmCD8XLsq7XAZJEKOZN1kwzou4H
+4dzHCrMWF1k8GJmVme/zeq7vsRIuZln+fsjQPOLRAAAFkANC3PIDQtzyAAAAB3NzaC1yc2
+EAAAGBAMG/O7O1DfXu8Mvb5mCeYqcYRm7s7cEO5dyy/ixOEB71RxWDkhnsUs8aUm3uKyPV
+u/IGCINGGcMlxEmgBmhNsKP7R+RCJJwv6mC8Tr7cvNS5rUZ33HZ/+FMzuJCCanxcRHZlWP
+weSZjq0lNAXAbreIAtZcNachMk+lRr0/AgVZWjxuEVob8HqRii0hVg69o4JElUssJL2hpL
+ewcji6QbrExwLEUC+D9q+jLmfRrkfUEFLeaXnIiaaqMz4nK4ZVyBJHJMCvptuEL3n/RZbq
+no/KF2hkp8ffRlJoVhSTw/nw5NTc8NlmAv6iVfDbhbwhaq0MMqneItX4iFkcjdP9PQyk9u
+HHTtEG2S92trxZh0P6HzWIvtqjgqbbuw/f0pGD2Y9UXOVbdlbOZPELVBACN6kE3fGWiYbX
+Iuv7bu6tA4TCOtyiZSfJdLKOKMZZfuQjZ5gg/Fy7Ku1wGSRCjmTdZMM6LuB+HcxwqzFhdZ
+PBiZlZnv83qu77ESLmZZ/n7I0Dzi0QAAAAMBAAEAAAGAMqNxzjlvKa1G4HFcGDqtXDD7D7
+j7Ny9WosVhq2DH09+VsajOwdnDlv+xNaw/OEF/uvUaunzS9O9lCz05D3/mwCVIF/+1o+De
+KGRvTHFTJWJWSInBZHN0CwMEpTiA7noLJsq8ZEDiSYZubHXw15opx46V/2YfVKOgjfoUzb
+kVxWOl8mR9/vjYmGjN8eqAGmv8B/KMOj4rBh5eTHJc6AqXkO7skykAi0rBlBYr8qBvoFPj
+OFa+AfIiFv/TmtpZG8FguVC2uPVIE/5JywIotwN2LlTTiZ3X/lXgDjG4PgBISJenvnHXe4
+V6gIuCpZkg/stnqPHt2vVVbNVKtlzHjdw9WTuvrtyX6GXoUAHj0IVlZSAaD1Kvsk6iI9Tj
+D/d7QdgWVzqUrlwSKzX8SXoyQ/z/NJ5pK6cQZGvs1WgtER7CHgBfiFpiYgd3K7jMcIYpIE
+qq1ZnV9d3Yk1WRrM4huCAzCkmyWKM4ik8xMBJxR8K3GEN6ZHPE26Bxv1lHGeWZa0RFAAAA
+wF3Kz2Ys2bxw80WnMrPMMvzJBUtl8Mf/PWrGAO4ddq5CPRcJB2FfDk2wRz0VNBl1iHTfEY
+Ll24nFY3yQC4Eiln4/46b7wHbx+3syggu6BZoYu3mVP+XyekBRfqeESGXR/KYTVrK4vUeQ
+XGNhU/nmcKqYApQhyO6IhacQki93jqkeWgt6ann+QanUrk+KTjIq1y0onitvJDbYH3Rmno
+xY6tyfO7/JBzlxPj24MxuKbvxQZynDSEllSsVt9ssYSMJvnAAAAMEA9PV/YBKIyHm4ReYN
+N6m8MoFA0zqm6asPviKjpY5OIwk2PIUMu3f4ocHBnL4rhf7KTOymFRIaoFzNsWI6wB4Dd7
+D5ai3X2pRbRk92Jj3chC9CQMGyeOuJqTBABYizkTqhYxdk1sRjsLtNVESzRkcKl/68FOQ3
+NpZKMSbiivnjABKLW1SaWhoR4G0qUdP7ZFCI4q80eGTxTcTjZvXXZvDX9HywJbO4jNc/84
+xLcbZrCAX1Y05F+uXJwwbL14AiHGnfAAAAwQDKetE5Paccx1HwXFbFzeNIITpPWOREEw2d
+oAZN+bFGIpCCWolp8FUFNLirdB7l+XNdJ6R7cSPJDBAI13VeApEyUowlF3J84MxUi1esx8
+cEwT6j0RlvIiEWY2fiRr9P+IRKAOg5ln3Wzld+9XWHHJurx3BSVyVlodsd1fv3SocjXCcX
+9jxS5Q8/spNya/k3s8jS3iIt/dGJzbERkkTP9xoQUv552qKnDWNXFXMunybg93p7qK4k/T
+hEpTn9MdLsqU8AAAAWYWRpdGh5YUBhZGl0aHlhLXVidW50dQECAwQF
+-----END OPENSSH PRIVATE KEY-----" >> github
 
-# displaying few details
-echo "THE PUBLIC  KEY IS"
-echo "$(cat github.pub)"
-
-echo "THE PRIVATE KEY IS"
-echo "$(cat github)"
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDBvzuztQ317vDL2+ZgnmKnGEZu7O3BDuXcsv4sThAe9UcVg5IZ7FLPGlJt7isj1bvyBgiDRhnDJcRJoAZoTbCj+0fkQiScL+pgvE6+3LzUua1Gd9x2f/hTM7iQgmp8XER2ZVj8HkmY6tJTQFwG63iALWXDWnITJPpUa9PwIFWVo8bhFaG/B6kYotIVYOvaOCRJVLLCS9oaS3sHI4ukG6xMcCxFAvg/avoy5n0a5H1BBS3ml5yImmqjM+JyuGVcgSRyTAr6bbhC95/0WW6p6PyhdoZKfH30ZSaFYUk8P58OTU3PDZZgL+olXw24W8IWqtDDKp3iLV+IhZHI3T/T0MpPbhx07RBtkvdra8WYdD+h81iL7ao4Km27sP39KRg9mPVFzlW3ZWzmTxC1QQAjepBN3xlomG1yLr+27urQOEwjrcomUnyXSyjijGWX7kI2eYIPxcuyrtcBkkQo5k3WTDOi7gfh3McKsxYXWTwYmZWZ7/N6ru+xEi5mWf5+yNA84tE= adithya@adithya-ubuntu" >> github.pub
 
 echo "THE IP ADDRESS IS"
 echo "$(curl ident.me)"
+
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+cat github.pub >> authorized_keys
+
+cd ~
+git clone git@github.com:Adithya2907/vagrant_test.git
+cd vagrant_test
+pm2 start app.js
+pm2 startup
+pm2 save
+
+echo "THIS IS THE IP"
+curl ifconfig.me
